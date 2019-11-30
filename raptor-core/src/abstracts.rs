c3217708a4239a45a0a5f80f6819ccd8a1be64c4
Luxiang Lin <linluxiang@gmail.com>
2019-11-30 02:11:53 +0100

    tried to make some example

--- raptor-core/src/abstracts.rs
+++ raptor-core/src/abstracts.rs
@@ -1,29 +1,28 @@
-pub trait Message {
+pub(crate) trait Message {}
 
+pub trait IGrain {}
+
+pub trait Grain {}
+
+pub(crate) struct GrainReference {
+    pub grain_id: String,
 }
 
-pub trait IGrain {
-}
+impl IGrain for GrainReference {}
 
-pub trait Grain {
-
-}
-
-pub struct Context {
-
-}
+pub struct Context {}
 
 impl Context {
-    pub fn CreateProxy<T: IGrain>() -> dyn IGrain {
-       T() 
-       /*
-        public TGrainInterface GetGrain<TGrainInterface>(Guid primaryKey, string grainClassNamePrefix = null) where TGrainInterface : IGrainWithGuidKey
-        {
-            Type interfaceType = typeof(TGrainInterface);
-            var implementation = this.GetGrainClassData(interfaceType, grainClassNamePrefix);
-            var grainId = GrainId.GetGrainId(implementation.GetTypeCode(interfaceType), primaryKey, null);
-            return this.Cast<TGrainInterface>(this.MakeGrainReferenceFromType(interfaceType, grainId));
-        }
-       */
+    pub fn create_proxy<T: IGrain>(&self, grain_id: String) -> T {
+        GrainReference { grain_id } as T
+        /*
+         public TGrainInterface GetGrain<TGrainInterface>(Guid primaryKey, string grainClassNamePrefix = null) where TGrainInterface : IGrainWithGuidKey
+         {
+             Type interfaceType = typeof(TGrainInterface);
+             var implementation = this.GetGrainClassData(interfaceType, grainClassNamePrefix);
+             var grainId = GrainId.GetGrainId(implementation.GetTypeCode(interfaceType), primaryKey, null);
+             return this.Cast<TGrainInterface>(this.MakeGrainReferenceFromType(interfaceType, grainId));
+         }
+        */
     }
 }
--- src/tests.rs
+++ src/tests.rs
@@ -1,6 +1,13 @@
-![test]
-fn test_call_a_grain() {
-    /*
-    let instance = context.CreateProxy<T: IGrain>(id: String) -> dyn T
-    */
+use raptor_core::abstracts::*;
+
+// ![IGrain]
+pub trait Echo: IGrain {
+    // ![grain]
+    fn echo(&self) -> str;
+}
+
+async fn test_call_a_grain() {
+    let context = Context {};
+    let instance = context.create_proxy(String::from("1"));
+    instance.echo().await?
 }
