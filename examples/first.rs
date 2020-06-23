f7c8651178ff37344162890af9c85d2460d0fc56
Luxiang Lin <linluxiang@gmail.com>
2020-06-24 00:30:51 +0200

    multiple ways of implemetation

--- examples/first.rs
+++ examples/first.rs
@@ -2,6 +2,24 @@ use raptor::core::clustering::server;
 use std::error::Error;
 
 #[tokio::main]
-async fn main() -> Result<(), impl Error> {
-    server::Server::default().start().await
+async fn main() -> Result<(), Box<dyn Error>> {
+    // Way 1
+    server::Server::default().start().await?;
+    Ok(())
+    // Way 2
+    // match server::Server::default().start().await {
+    //     Ok(()) => {
+    //         Ok(())
+    //     }
+    //     Err(e) => {
+    //         Err(e.into())
+    //     }
+    // }
+    // Way 3
+    // let r = server ::Server::default().start().await;
+    // match r {
+    //     OK(k) => {
+    //         Ok(k.into())
+    //     }
+    // }
 }
